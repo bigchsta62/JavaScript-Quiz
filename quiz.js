3911a1ce823c7d21c7f97598fff04086e1e37685
bigchsta62 <bigchsta62@msn.com>
2020-06-28 22:00:05 -0700

    got the add answer button function working

--- quiz.js
+++ quiz.js
@@ -1,6 +1,7 @@
 const startButton = document.getElementById('start-btn');
-
-const questionElement = document.getElementById('question-container')
+const nextButton = document.getElementById('next-btn');
+const qContainer = document.getElementById('question-container');
+const qElement = document.getElementById('question')
 const aBtnsElement = document.getElementById('answer-buttons');
 
 
@@ -9,12 +10,13 @@ let shuffledQs, currentQs
 startButton.addEventListener('click', startGame);
 
 
+
 function startGame() {
     console.log('startGame works');
     startButton.classList.add('hide');
     shuffledQs = questions.sort(() => Math.random() - .5);
     currentQs = 0
-    questionElement.classList.remove('hide');
+    qContainer.classList.remove('hide');
     nextQuestion();
 }
 
@@ -23,22 +25,32 @@ function nextQuestion() {
 }
 
 function showQ(question) {
-questionElement.innerText = question.question
+    qElement.innerText = question.question
+    question.answers.forEach(answer => {
+    const button = document.createElement('button');
+    button.innerText = answer.text
+    button.classList.add('btn', 'btn-outline-secondary');
+    if (answer.correct) {
+        button.dataset.correct = answer.correct;
+    }
+    button.addEventListener('click', selectAnswer)
+    aBtnsElement.appendChild(button);
+})
 }
 
-function selectAnswer() {
+function selectAnswer(e) {
 
 }
 
 const questions = [
     {
-        question: 'What is 2+2',
+        question: 'What is 2+2?',
         answers: [
             { text: '4', correct: true },
             {text: '22', correct: false }
         ]
     }
-];
+]
 
 
 // const question1 = ('Is JavaScript is a full-fledged dynamic ___________________ that can add interactivity to a website?');
