82c26236fefa5f0a2d5a282145b5599739a03de3
bigchsta62 <bigchsta62@msn.com>
2020-06-30 16:52:38 -0700

    mostly working, still ugly

--- quiz.js
+++ quiz.js
@@ -1,30 +1,50 @@
 const startButton = document.getElementById('start-btn');
 const nextButton = document.getElementById('next-btn');
-const highScoresBtn = document.getElementById('high-scores-btn');
 const qContainer = document.getElementById('question-container');
 const qElement = document.getElementById('question')
 const aBtnsElement = document.getElementById('answer-buttons');
 
+const lastScore = localStorage.setItem('lastScore', []);
 const scoreText = document.getElementById('score');
 const correctPoints = 10;
+const wrongPoints = 0;
 
+//I don't know why the const downLaoadTimer works
+//, but it made the last question go away and show
+// the High Scores button, even with time left.
+//Without it, the last questions would stay until the timer ran out.
+const downloadTimer = 'setInterval()';
 let shuffledQs, currentQs;
 let score = 0;
+let timeleft = 59;
+
 
 
 startButton.addEventListener('click', () => {
     startGame();
+    //quiz timer
+    setInterval(function () {
+        if (timeleft < 0) {
+            localStorage.setItem('lastScore', score);
+            clearInterval(downloadTimer);
+            document.getElementById("countdown");
+            return window.location.assign('/scores.html');            
+        } else {
+            document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+        };
+        timeleft -= 1;
+    }, 1000);
 });
 nextButton.addEventListener('click', () => {
     currentQs++;
     nextQuestion();
-    
+
 });
 
-// starts the Quiz
+// function to be called to start the quiz
 function startGame() {
     // console.log('startGame works');
-    
+
     score = 0;
     startButton.classList.add('hide');
     shuffledQs = questions.sort(() => Math.random() - .5);
@@ -33,12 +53,14 @@ function startGame() {
     nextQuestion();
     // startTimer();
 };
+
 // starts at random question each time the quiz is restarted
 function nextQuestion() {
     resetJumbo()
     // resetTimer()
     showQ(shuffledQs[currentQs]);
 };
+
 // shows the current question and corresponding answers
 function showQ(question) {
     qElement.innerText = question.question
@@ -53,6 +75,7 @@ function showQ(question) {
         aBtnsElement.appendChild(button);
     });
 };
+
 //resets the answer buttons
 function resetJumbo() {
     resetRW(document.body);
@@ -64,22 +87,24 @@ function resetJumbo() {
 };
 
 
-//quiz timer
-let timeleft = 15;
 
-const downloadTimer = setInterval(function () {
-    if (timeleft < 0) {
-        clearInterval(downloadTimer);
-        document.getElementById("countdown").innerHTML = "Out of Time!";
-        qContainer.classList.add('hide');
-        highScoresBtn.classList.remove('hide');
-        
 
-    } else {
-        document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
-    };
-    timeleft -= 1;
-}, 1000);
+// I found out on stack overflow, that I needed to call my timer inside
+// the startButton click event or it would start as soon as the script loaded.
+
+// const downloadTimer = setInterval(function () {
+//     if (timeleft < 0) {
+//         clearInterval(downloadTimer);
+//         document.getElementById("countdown");
+//         qContainer.classList.add('hide');
+//         highScoresBtn.classList.remove('hide');
+
+
+//     } else {
+//         document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+//     };
+//     timeleft -= 1;
+// }, 1000);
 
 
 
@@ -94,14 +119,11 @@ function answerPick(e) {
     if (shuffledQs.length > currentQs + 1) {
         nextButton.classList.remove('hide');
     } else {
-        const aTag = document.createElement('a');
-        startButton.innerText = '';
-        qContainer.classList.add('hide');
-        highScoresBtn.classList.remove('hide');
+        clearInterval(downloadTimer);
+        localStorage.setItem('lastScore', score);   
+        return window.location.assign('scores.html');
     };
 };
-
-
 
 
 function rightOrWrong(element, correct) {
@@ -111,7 +133,7 @@ function rightOrWrong(element, correct) {
         incrementScore(correctPoints);
     } else {
         element.classList.add('wrong');
-
+        timeleft -= 1;  
     }
 };
 
@@ -173,31 +195,3 @@ incrementScore = (num) => {
 
 
 
-const username = document.getElementById('username');
-const saveScoreBtn = document.getElementById('saveScoreBtn');
-const finalScore = document.getElementById('finalScore');
-const lastScore = localStorage.getItem('lastScore');
-
-
-
-const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
-console.log(highScores);
-console.log(JSON.parse(localStorage.getItem('highScores')));
-finalScore.innerText = lastScore;
-
-username.addEventListener('keyup', () => {
-    console.log(username.value);
-    saveScoreBtn.disabled = !username.value;
-});
-
-saveScore = e => {
-    console.log('clicked save btn');
-    event.preventDefault();
-
-
-    const score = {
-        score: lastScore,
-        name: username.value
-    };
-    console.log(score);
-};
--- scores.js
+++ scores.js
@@ -1,28 +1,57 @@
-// const username = document.getElementById('username');
-// const saveScoreBtn = document.getElementById('saveScoreBtn');
-// const finalScore = document.getElementById('finalScore');
-// const lastScore = localStorage.getItem('lastScore');
+//This page was influenced by James Q Quick https://www.youtube.com/channel/UC-T8W79DN6PBnzomelvqJYw and his Youtube videos on a guiz game 
+
+const username = document.getElementById('username');
+const saveScoreBtn = document.getElementById('saveScoreBtn');
+const finalScore = document.getElementById('finalScore');
+const lastScore = localStorage.getItem('lastScore');
+const topFiveList = document.getElementById('topFiveList');
+const highScores = JSON.parse(localStorage.getItem('highScores'));
 
 
+//I havent been able to get this code to completely work. Score is not selected
+//and it returns the values incorrectly onto the page
+topFiveList.innerHTML = highScores
+.map(score => {
+   return '<li class="high-score">${score.name}-${score.score}</li>'
+}).join('')
+
+
+console.log('highScores');
+
+
+
+
+finalScore.innerText = lastScore;
+
+
+
+
+const topFive = 5;
 
 // const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
-// console.log(highScores);
+
 // console.log(JSON.parse(localStorage.getItem('highScores')));
-// finalScore.innerText = lastScore;
+finalScore.innerText = lastScore;
 
-// username.addEventListener('keyup', () => {
-//     console.log(username.value);
-//     saveScoreBtn.disabled = !username.value;
-// });
+username.addEventListener('keyup', () => {
+    // console.log(username.value);
+    saveScoreBtn.disabled = !username.value;
+});
 
-// saveScore = e => {
-//     console.log('clicked save btn');
-//     event.preventDefault();
+saveScore = e => {
+    console.log('clicked save btn');
+    e.preventDefault();
 
+    const score = {
+        score: Math.floor(Math.random() * 100),
+        name: username.value
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
 
-//     const score = {
-//         score: lastScore,
-//         name: username.value
-//     };
-//     console.log(score);
-// };
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    // window.location.assign('/');
+
+};
+
