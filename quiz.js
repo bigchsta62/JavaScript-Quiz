0624b3ab51ebf311ef6c1f4ca98edd04b70254e2
bigchsta62 <bigchsta62@msn.com>
2020-06-29 23:51:47 -0700

    getting time implemented

--- quiz.js
+++ quiz.js
@@ -5,31 +5,36 @@ const qElement = document.getElementById('question')
 const aBtnsElement = document.getElementById('answer-buttons');
 
 
-let shuffledQs, currentQs
+let shuffledQs, currentQs;
 let countRightAns = 0;
 
-startButton.addEventListener('click', startGame);
+startButton.addEventListener('click', () => {
+    startGame();
+});
 nextButton.addEventListener('click', () => {
     currentQs++;
-    nextQuestion()
-})
+    nextQuestion();
+    setInterval();
+});
 
-
+// starts the Quiz
 function startGame() {
     // console.log('startGame works');
-    countRightAns = 0;
+
     startButton.classList.add('hide');
     shuffledQs = questions.sort(() => Math.random() - .5);
     currentQs = 0
     qContainer.classList.remove('hide');
     nextQuestion();
-}
-
+    // startTimer();
+};
+// starts at random question each time the quiz is restarted
 function nextQuestion() {
     resetJumbo()
+    // resetTimer()
     showQ(shuffledQs[currentQs]);
-}
-
+};
+// shows the current question and corresponding answers
 function showQ(question) {
     qElement.innerText = question.question
     question.answers.forEach(answer => {
@@ -41,17 +46,38 @@ function showQ(question) {
         }
         button.addEventListener('click', answerPick)
         aBtnsElement.appendChild(button);
-    })
-}
-
+    });
+};
+//resets the answer buttons
 function resetJumbo() {
     resetRW(document.body);
     nextButton.classList.add('hide');
     while (aBtnsElement.firstChild) {
         aBtnsElement.removeChild
             (aBtnsElement.firstChild)
-    }
-}
+    };
+};
+
+
+//quiz timer
+let timeleft = 15;
+
+const downloadTimer = setInterval(function () {
+    if (timeleft < 0) {
+        clearInterval(downloadTimer);
+        document.getElementById("countdown").innerHTML = "Out of Time!";
+        qContainer.classList.add('hide');
+        startButton.innerText = 'Try Again';
+        startButton.classList.remove('hide');
+
+    } else {
+        document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+    };
+    timeleft -= 1;
+}, 1000);
+
+
+
 
 function answerPick(e) {
     const clickedBtn = e.target;
@@ -63,30 +89,31 @@ function answerPick(e) {
     if (shuffledQs.length > currentQs + 1) {
         nextButton.classList.remove('hide');
     } else {
-        startButton.innerText = 'Game Over';
+        startButton.innerText = 'High Scores';
         startButton.classList.remove('hide');
-    }
-    // if (clickedBtn.dataset = correct) {
-    //     countRightAns++;
-    //  // +1, change it if you need +10, +25 etc
-    //  }
-    //  document.getElementById('right-answers').innerHTML = countRightAns;
+        qContainer.classList.add('hide');
+        clearTimeout(downloadTimer);
+    };
+};
 
-}
+
+
 
 function rightOrWrong(element, correct) {
     resetRW(element);
     if (correct) {
         element.classList.add('correct');
+        aBtnsElement.disabled = true;
     } else {
         element.classList.add('wrong');
+
     }
-}
+};
 
 function resetRW(element) {
     element.classList.remove('correct');
     element.classList.remove('wrong');
-}
+};
 
 const questions = [
     {
@@ -123,7 +150,7 @@ const questions = [
         ]
     },
     {
-        question: 'Which of the follwing would link an HTML page to Javascrip',
+        question: 'Which of the follwing would link an HTML page to Javascript',
         answers: [
             { text: 'put that Java over here', correct: false },
             { text: '<link rel="stylesheet" href="style.css">', correct: false },
@@ -135,27 +162,6 @@ const questions = [
 ]
 
 
-// const question1 = ('Is JavaScript is a full-fledged dynamic ___________________ that can add interactivity to a website?');
-// const question2 = ('');
-// const question3 = ('');
-// const question4 = ('');
 
-// // for (let i = 0; i < array.length; i++) {
-// //     const element = array[i];
-
-// // }
-
-
-
-// const answers = ('programming language');
-
-
-
-// for (let i = 0; i < answers; i++) {
-//     const answerBtn = document.createElement('BUTTON');
-//     answerBtn.setAttribute('choices', answers[i]);
-//     document.getElementById('ansBtns').appendChild(answerBtn);
-
-// }
 
 
